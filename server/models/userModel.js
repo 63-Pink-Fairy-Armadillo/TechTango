147ea5f14a2284e5ae93f951006161439b4f00a9
Stan Louie <stanlouie@Stans-MBP.lan>
2024-02-17 13:47:27 -0800

    StanL 02/17/2024 Justin-Stan-BackEnd login and signup backend set up dquote> dquote> dquote> Co-author-by: Justin <hozion612356@gmail.com> dquote> Co-author-by: Jacob <jmcfarlandemail@gmail.com> dquote> Co-author-by: Quan <jeanquan98@gmail.com>

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,9 +1,38 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 
-// const userSchema = new Schema({
-//   username: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-// });
+const SALT_WORK_FACTOR = 10;
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+accountSchema.pre('save', function (next) {
+  const account = this;
+
+  // generate a salt through SALT_WORK_FACTOR
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next({
+      log: 'Express error handler caught error in userModel bcrypt.genSalt function',
+      status: 500,
+      message: {err},
+    });
+
+    // hash the password using new salt
+    bcrypt.hash(account.password, salt, function (err, hash) {
+      if (err) return next({
+        log: 'Express error handler caught error in userModel bcrypt.hash function',
+        status: 500,
+        message: {err},
+      });
+
+      // overwrite the plaintext password with the hashed one
+      account.password = hash;
+      next();
+    });
+  });
+});
 
 module.exports = mongoose.model('User', userSchema);
--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const path = require('path');
-// const mongoose = require('mongoose');
-//hello world!
-const PORT = 3000;
+const mongoose = require('mongoose');
 
+const userController = require('./controllers/userController');
+const MONGO_URI = 'mongodb+srv://justin:a1357924689@cluster0.se1vl.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(MONGO_URI);
+
+const PORT = 3000;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.resolve(__dirname, '../client')));
+
+app.post('/signUp', userController.createUser, (req, res) => {
+  return res.status(200).json(res.locals.newUser);
+})
+
+app.post('/signIn', userController.verifyUser, (req, res) => {
+  return res.status(200).json(res.locals.user);
+})
 
 /**
  * 404 handler
